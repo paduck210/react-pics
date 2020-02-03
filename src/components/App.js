f69615fb03d6856cc297e99e075dd1a284a2618f
Jung Yu <hjyu1408@gmail.com>
2020-02-03 02:55:59 -0500

    Connect term with Unsplash.api

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import SearchWord from './SearchWord';
 
 class App extends React.Component {
-  state = { data: '' };
-
-  callbackFunction = childData => {
-    this.setState({ data: childData });
-  };
+  onSearchSubmit(term) {
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: { Authorization: process.env.REACT_APP_UNSPLASH_ID },
+    });
+  }
 
   render() {
     return (
       <div className="container" style={{ margin: '50px' }}>
-        <SearchWord onSubmit={this.callbackFunction} />
-        <h1>{this.state.data}</h1>
+        <SearchWord onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
--- src/components/SearchWord.js
+++ src/components/SearchWord.js
@@ -3,10 +3,6 @@ import React from 'react';
 class SearchWord extends React.Component {
   state = { input: '' };
 
-  handleChange = e => {
-    this.setState({ input: e.target.value });
-  };
-
   handleSubmit = e => {
     e.preventDefault();
     this.props.onSubmit(this.state.input);
@@ -20,7 +16,7 @@ class SearchWord extends React.Component {
             <label>User Input</label>
             <input
               type="text"
-              onChange={this.handleChange}
+              onChange={e => this.setState({ input: e.target.value })}
               value={this.state.term}
             />
           </div>
