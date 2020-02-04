2a32ad29e06184d21a6b9e911653b01f41510ac0
Jung Yu <hjyu1408@gmail.com>
2020-02-04 00:38:31 -0500

    Store API data as state and set Promise
    - Make state
    - Set Promise function using async and await
    - Change function to arrow function to point right this

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,22 @@ import axios from 'axios';
 import SearchWord from './SearchWord';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: { Authorization: process.env.REACT_APP_UNSPLASH_ID },
     });
-  }
+    // console.log(this);
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="container" style={{ margin: '50px' }}>
         <SearchWord onSubmit={this.onSearchSubmit} />
+        <h1>{this.state.images}</h1>
       </div>
     );
   }
